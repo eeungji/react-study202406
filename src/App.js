a232907a36518d1f2967f755b2f26eef5604c9ae
kimeunji <eunji93121@gmail.com>
2024-06-13 23:57:08 +0900

    #jsx 기초

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// App 이라는 이름으로 태그들이 묶여있음.
+// 컴포넌트라고 부름 태그들을 묶어놓고 있음.
 
 function App() {
+
+  // jsx 문법
+  // const $h2 = React.createElement('h2', null, '방가방가햄토리');
+  const $h2 = <h2>방가방가햄토리</h2>;
+
+  // jsx 규칙
+  // 1. return안에 있는 태그는 반드시 하나의 태그로 묶어야 함.
+  // 2. 빈 태그(닫는 태그가 없는)는 반드시 />로 마감
+  // 3. 태그의 class 속성은 자바스크립트 키워드 class와 겹쳐서 className으로 표기
+  // 자바스크립트 랜더링 될 때는 className이 브라우저에서 class로 바뀌어서 랜더링됨.
+  // 4. 의미없는 부모는 <React.Fragment>로 감싸면 됨
+  // 5. 변수값이나 함수를 출력할 때는 {}로 감싸면 됨
+
+  const hello = '안녕안녕?';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>{hello}</h1>
+        {$h2}
+      </div>
+      <div className='noname'>
+        <input type="text" />
+        <label htmlfor=""></label>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
+
+// 22행 중괄호는 js에 없는 특별한 문법
+// return은 태그 여러개 보낼 수 없음.
+// 형제 태그가 있는 경우 반드시 부모태그 하나로 감싸줘야 함
